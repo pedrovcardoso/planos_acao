896d3023737d4d435823fdc1c47980f91b17c9bf
Pedro Cardoso <pedrovieirapessoal@gmail.com>
2025-09-09 11:11:05 -0300

    removendo logo do sei do card

--- pages/planos/scripts.js
+++ pages/planos/scripts.js
@@ -498,10 +498,7 @@ function gerarCards(jsonPlanos) {
           <h2 class="pr-8 text-xl font-bold text-sky-700 tracking-wide">${plano.Nome}</h2>
 
           <div class="mt-3 space-y-1 text-sm text-slate-600">
-            <div class="flex items-center gap-2">
-              <img src="../../assets/images/logo_sei_mg.png" class="w-10 h-10 object-contain filter grayscale-[40%]" alt="Logo SEI">
-              <span>${plano["Processo SEI"]}</span>
-            </div>
+            <p><strong class="font-semibold text-slate-700">Processo SEI:</strong> ${plano["Processo SEI"]}</p>
             <p><strong class="font-semibold text-slate-700">Documento TCE:</strong> ${plano["Documento TCE"]}</p>
           </div>
         </div>
@@ -535,13 +532,7 @@ function gerarCards(jsonPlanos) {
                 </ul>
               </div>
 
-              <div class="flex items-center gap-2 mt-2">
-                <img 
-                  src="../../assets/images/logo_sei_mg.png" 
-                  class="w-10 object-contain filter grayscale-[40%]" 
-                  alt="Logo SEI">
-                <strong class="font-medium text-slate-600">relacionados:</strong>
-              </div>
+              <p><strong class="font-medium text-slate-600">SEI relacionados:</strong> ${plano['SEI relacionados'] || '-'}</p>
               <span>
                 ${plano['SEI relacionados'] || '-'}
               </span>
